06b84fb467d02f5378d300dc2c077aeb3f33af39
Khalid Saifullah <mdkhalid57470@gmail.com>
2025-12-10 21:30:08 +0530

    fixed production bugs

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -3,7 +3,11 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(process.cwd(), "public/temp"))
+    if (process.env.NODE_ENV === "production") {
+      cb(null, "/tmp");
+    } else {
+      cb(null, path.join(process.cwd(), "public/temp"));
+    }
   },
   filename: function (req, file, cb) {
 
